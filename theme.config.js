9deadbb25a6497c9ce95007606393c67d8c0030b
Samael3003 <103866475+Samael3003@users.noreply.github.com>
2022-10-09 00:17:13 +0530

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,7 +1,7 @@
 export default {
   github: 'https://github.com/shuding/nextra',
   docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  titleSuffix: ' – Samael3003',
   logo: (
     <>
       <span className="mr-2 font-extrabold hidden md:inline">SAMAEL</span>
