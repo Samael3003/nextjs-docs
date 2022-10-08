38d31a450d9cc544e6d93da3f33fa1b5aaa85243
Samael3003 <103866475+Samael3003@users.noreply.github.com>
2022-10-09 00:16:47 +0530

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -4,9 +4,9 @@ export default {
   titleSuffix: ' – Nextra',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">SAMAEL</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        The Fallen Programmer
       </span>
     </>
   ),
